a79717ca95a58e14861ca89277e6b88227a15e57
Hossam Al Mustafa <128149196+Samustafa@users.noreply.github.com>
2023-04-02 12:56:23 +0200

    EL-24: Created a sytles file

--- Extension/src/pages/popup/Not_Authenticated/LandingPage.tsx
+++ Extension/src/pages/popup/Not_Authenticated/LandingPage.tsx
@@ -1,10 +1,11 @@
+import {buttonStyle} from "@pages/popup/Styles";
 
 export default function LandingPage(): JSX.Element {
     return (
         <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
             <h2>welcome to the EI_Logger</h2>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Log In</button>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Register</button>
+            <button className={buttonStyle}>Log In</button>
+            <button className={buttonStyle}>Register</button>
         </div>
     );
 }
