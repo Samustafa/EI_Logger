cd643106518a5ef8fafdb902e1e55ac5268f29cb
Hossam Al Mustafa <128149196+Samustafa@users.noreply.github.com>
2023-04-04 11:29:36 +0200

    EL-24: deleted Tests and added navigator to LandingPage.tsx

--- Extension/src/pages/popup/Not_Authenticated/LandingPage.tsx
+++ Extension/src/pages/popup/Not_Authenticated/LandingPage.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import {buttonStyle} from "@pages/popup/Consts/Styles";
+import {useNavigate} from "react-router-dom";
+import Paths from "@pages/popup/Consts/Paths";
 
 export default function LandingPage(): JSX.Element {
+    const navigate = useNavigate();
     return (
         <>
             <h2>Welcome to the EI_Logger</h2>
-            <button className={buttonStyle} onClick={() => {
-            }}>Register
-            </button>
+            <button className={buttonStyle} onClick={() => navigate(Paths.registrationPage)}>Register</button>
         </>
     );
 }
