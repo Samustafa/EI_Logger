60d1517999efd08dcd1e116e686d2a492d8a00f2
Hossam Al Mustafa <128149196+Samustafa@users.noreply.github.com>
2023-04-05 16:38:57 +0200

    EL-24:  added shallow testing lib added tests for LoadingButton.test.tsx

---
+++ Extension/src/pages/__test__/SharedComponents/LoadingButton.test.tsx
@@ -0,0 +1,24 @@
+import {describe, expect, test} from "vitest";
+import React from "react";
+import {LoadingButton} from "@pages/popup/SharedComponents/LoadingButton";
+import {cleanup, render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+
+describe("LoadingButton", () => {
+    beforeEach(() => cleanup());
+    test("renders correctly", () => {
+        render(<LoadingButton text="Test Button" loadingText="Loading..." isLoading={false}/>);
+        expect(screen).toBeTruthy();
+    });
+    test("renders loadingText when isLoading is true", () => {
+        render(<LoadingButton text="Test Button" loadingText="Loading..." isLoading={true}/>);
+        expect(screen.getByText('Loading...')).toBeInTheDocument();
+        expect(screen.getByText('Test Button')).not.toBeInTheDocument();
+    });
+    test("renders text when isLoading is false", () => {
+        render(<LoadingButton text="Test Button" loadingText="Loading..." isLoading={false}/>);
+        expect(screen.getByText('Test Button')).toBeInTheDocument();
+        expect(screen.getByText('Loading...')).not.toBeInTheDocument();
+    });
+});
