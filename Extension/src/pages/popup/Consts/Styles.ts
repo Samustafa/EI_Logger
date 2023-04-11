fc65d1e70c58c1daa0049e7d326be504f1e47f65
Hossam Al Mustafa <128149196+Samustafa@users.noreply.github.com>
2023-04-11 21:40:23 +0200

    EL-24:  Updated Error text style

--- Extension/src/pages/popup/Consts/Styles.ts
+++ Extension/src/pages/popup/Consts/Styles.ts
@@ -4,4 +4,4 @@ export const buttonDisabledStyle = "bg-gray-300 text-white font-bold py-2 px-4 r
 export const registrationLabelStyle = "block text-gray-700 text-sm font-bold mb-2";
 export const inputDefaultStyle = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline";
 export const inputErrorStyle = "shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline";
-export const errorDivStyle = "bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative";
+export const errorDivStyle = "bg-red-100 border border-red-400 text-red-700 rounded relative";
