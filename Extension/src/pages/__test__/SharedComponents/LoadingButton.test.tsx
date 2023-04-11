bb7a9dc361b60e64bc35987fc4f212c882f04a38
Hossam Al Mustafa <128149196+Samustafa@users.noreply.github.com>
2023-04-11 21:31:08 +0200

    EL-24:  Loading LoadingButton.test.tsx complete

--- Extension/src/pages/__test__/SharedComponents/LoadingButton.test.tsx
+++ Extension/src/pages/__test__/SharedComponents/LoadingButton.test.tsx
@@ -1,8 +1,9 @@
-import {describe, expect, test} from "vitest";
+import {describe, expect, test, vitest} from "vitest";
 import React from "react";
 import {LoadingButton} from "@pages/popup/SharedComponents/LoadingButton";
 import {cleanup, render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 
 
 describe("LoadingButton", () => {
@@ -14,11 +15,29 @@ describe("LoadingButton", () => {
     test("renders loadingText when isLoading is true", () => {
         render(<LoadingButton text="Test Button" loadingText="Loading..." isLoading={true}/>);
         expect(screen.getByText('Loading...')).toBeInTheDocument();
-        expect(screen.getByText('Test Button')).not.toBeInTheDocument();
+        expect(screen.queryByText('Test Button')).not.toBeInTheDocument();
     });
     test("renders text when isLoading is false", () => {
         render(<LoadingButton text="Test Button" loadingText="Loading..." isLoading={false}/>);
         expect(screen.getByText('Test Button')).toBeInTheDocument();
-        expect(screen.getByText('Loading...')).not.toBeInTheDocument();
+        expect(screen.queryByText('Loading...')).not.toBeInTheDocument();
+    });
+    test("is disabled when isLoading is true", () => {
+        render(<LoadingButton text="Test Button" loadingText="Loading..." isLoading={true}/>);
+        const button = screen.getByText('Loading...');
+        expect(button).toBeDisabled();
+    });
+    test("is enabled when isLoading is false", () => {
+        render(<LoadingButton text="Test Button" loadingText="Loading..." isLoading={false}/>);
+        const button = screen.getByText('Test Button');
+        expect(button).not.toBeDisabled();
+    });
+    test("clicks", async () => {
+        render(<LoadingButton text="Test Button" loadingText="Loading..." isLoading={false}/>);
+        const button = screen.getByText('Test Button');
+        const user = userEvent.setup();
+        const clickSpy = vitest.spyOn(user, 'click');
+        await user.click(button);
+        expect(clickSpy).toHaveBeenCalledOnce();
     });
 });
