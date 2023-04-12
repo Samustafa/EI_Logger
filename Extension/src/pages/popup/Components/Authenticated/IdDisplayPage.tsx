1ddaf19a9ee7868ad680991be1b32e27083a5581
Hossam Al Mustafa <128149196+Samustafa@users.noreply.github.com>
2023-04-12 16:12:07 +0200

    EL-24-41: Created Copy Functionality to the id

--- Extension/src/pages/popup/Components/Authenticated/IdDisplayPage.tsx
+++ Extension/src/pages/popup/Components/Authenticated/IdDisplayPage.tsx
@@ -1,4 +1,6 @@
 import {useParams} from "react-router-dom";
+import CopyToClipboardButton from "@pages/popup/SharedComponents/CopyToClipboardButton";
+import React from "react";
 
 export function IdDisplayPage() {
     const {id} = useParams<string>();
@@ -7,11 +9,14 @@ export function IdDisplayPage() {
         <>
             <p>Registration Successful!</p>
             <br/>
-            <p>{id}</p>
+            <div className={"bg-green-300 border-double border-4 border-sky-500 grid grid-rows-1 grid-flow-col gap-4"}>
+                <p className="text-green-600 font-bold">{id}</p>
+                <CopyToClipboardButton textToCopy={id!}/>
+            </div>
+
             <br/>
             <p>Please save your ID somewhere safe and don't share it with any person, even the people responsible for
-                the
-                study!</p>
+                the study!</p>
             <p>You will need your ID, if you decide to log-in from another device!</p>
         </>
     );
