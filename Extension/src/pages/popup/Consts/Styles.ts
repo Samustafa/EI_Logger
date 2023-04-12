c477e218e123fb66ab0b395e03c9a614d113c25c
Hossam Al Mustafa <128149196+Samustafa@users.noreply.github.com>
2023-04-12 18:00:48 +0200

    EL-40: made the text lighter

--- Extension/src/pages/popup/Consts/Styles.ts
+++ Extension/src/pages/popup/Consts/Styles.ts
@@ -1,4 +1,4 @@
-export const generalStyle = "absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800"
+export const generalStyle = "absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800 text-gray-100 text-lg"
 export const buttonStyle = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ";
 export const buttonDisabledStyle = "bg-gray-300 text-white font-bold py-2 px-4 rounded";
 export const registrationLabelStyle = "block text-gray-700 text-sm font-bold mb-2";
